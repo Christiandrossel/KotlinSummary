6209eafd5496b55d1b3da9f1ccf2dac006e0e510
Christian <timmi0701@googlemail.com>
2025-09-02 13:52:58 +0200

    Add Google OAuth2 security configuration and update product repository with filtering method

--- ddd-example/src/main/java/org/example/repository/ProductRepository.kt
+++ ddd-example/src/main/java/org/example/repository/ProductRepository.kt
@@ -4,4 +4,7 @@ import org.example.controler.model.Product
 import org.springframework.data.mongodb.repository.MongoRepository
 
 interface ProductRepository: MongoRepository<Product, String>, ProductRepositoryExtension {
+
+    @org.springframework.data.mongodb.repository.Query("{ '\$or': [ { 'price': { '\$gt': ?0 } }, { 'tags': { '\$in': ?1 } } ], 'available': true }")
+    fun findFilteredProductsByPriceAndTax(minPrice: Double, tags: List<String>): List<Product>
 }
