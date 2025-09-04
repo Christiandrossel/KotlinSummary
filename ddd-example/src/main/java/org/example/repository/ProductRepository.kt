e905be61eab16065b6b12f667e342b4fb8c2b5c5
Christian <timmi0701@googlemail.com>
2025-09-04 12:31:05 +0200

    Add repository annotations to ProductRepository and UserRepository

--- ddd-example/src/main/java/org/example/repository/ProductRepository.kt
+++ ddd-example/src/main/java/org/example/repository/ProductRepository.kt
@@ -2,6 +2,8 @@ package org.example.repository
 
 import org.example.controler.model.Product
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface ProductRepository: MongoRepository<Product, String>, ProductRepositoryExtension {
 }
--- ddd-example/src/main/java/org/example/repository/UserRepository.kt
+++ ddd-example/src/main/java/org/example/repository/UserRepository.kt
@@ -2,7 +2,9 @@ package org.example.repository
 
 import org.example.repository.model.UserEntity
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface UserRepository: MongoRepository<UserEntity, String> {
 
     fun findByUsername(username: String): UserEntity?
